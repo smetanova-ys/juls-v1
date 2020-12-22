e6448bcf47376cda05125e28341bde8ea42e339e
smetanova-ys <smetanova.ys@gmail.com>
2020-12-22 17:54:09 +0100

    file js added

---
+++ menu.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const el = document.getElementsByClassName('menu__item');
+
+for(var i=0; i<el.length; i++) {
+    el[i].addEventListener("mouseenter", showSub, false);
+    el[i].addEventListener("mouseleave", hideSub, false);
+ }
+
+ function showSub(e) {
+    if(this.children.length>1) {
+       this.children[1].style.height = "auto";
+       this.children[1].style.overflow = "visible";
+       this.children[1].style.opacity = "1";
+    } else {
+       return false;
+    }
+ }
+
+ function hideSub(e) {
+    if(this.children.length>1) {
+      this.children[1].style.height = "0px";
+       this.children[1].style.overflow = "hidden";
+       this.children[1].style.opacity = "0";
+    } else {
+       return false;
+    }
+}
